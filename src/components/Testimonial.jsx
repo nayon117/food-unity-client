3f4bc72eac6bb2df7c572b2a5d9c8372f70aa132
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-05 18:44:18 +0600

    add testimonial section

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,96 @@
+import { BiSolidQuoteSingleLeft } from "react-icons/bi";
+const Testimonial = () => {
+  return (
+    <div className="py-16">
+      <div className="space-y-3">
+        <h2 className="text-xl md:text-2xl  lg:text-4xl text-center font-bold">
+          Stories of Impact
+        </h2>
+        <p className="text-center font-medium">
+          Discover how our community members are making a difference by sharing
+          surplus food and reducing waste. Read their inspiring stories and
+          learn how FoodUnity has transformed their food-sharing experiences.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
+        <div role="listitem" className="bg-white shadow rounded p-4 xl:p-8">
+          <BiSolidQuoteSingleLeft></BiSolidQuoteSingleLeft>
+          <div className="pl-4 pt-4 flex items-start justify-between">
+            <div className="mr-6">
+              <p className="xl:text-xl xl:leading-loose text-gray-600">
+                Sharing food through FoodUnity is easy and fulfilling. Its a
+                win-win
+              </p>
+              <p className="mt-4 text-base font-semibold leading-none text-gray-800">
+                Anna
+              </p>
+            </div>
+            <img
+              src="https://i.ibb.co/qRcWFfM/avatar-1.png"
+              alt="Display Avatar of Anna Smith"
+              role="img"
+            />
+          </div>
+        </div>
+        <div role="listitem" className="bg-white shadow rounded p-4 xl:p-8">
+          <BiSolidQuoteSingleLeft></BiSolidQuoteSingleLeft>
+          <div className="pl-4 pt-4 flex items-start justify-between">
+            <div className="mr-6">
+              <p className="xl:text-xl xl:leading-loose text-gray-600">
+                FoodUnity connects me to surplus food in my community
+                effortlessly.
+              </p>
+              <p className="mt-4 text-base font-semibold leading-none text-gray-800">
+                John
+              </p>
+            </div>
+            <img
+              src="https://i.ibb.co/vmqhQZ6/avatar-2.png"
+              alt="Display avatar of Dany John"
+              role="img"
+            />
+          </div>
+        </div>
+        <div role="listitem" className="bg-white shadow rounded p-4 xl:p-8">
+          <BiSolidQuoteSingleLeft></BiSolidQuoteSingleLeft>
+          <div className="pl-4 pt-4 flex items-start justify-between">
+            <div className="mr-6">
+              <p className="xl:text-xl xl:leading-loose text-gray-600">
+                FoodUnity: Where neighbors come together to reduce food waste.
+              </p>
+              <p className="mt-4 text-base font-semibold leading-none text-gray-800">
+                Mike
+              </p>
+            </div>
+            <img
+              src="https://i.ibb.co/znmdwyv/avatar-3.png"
+              alt="Display Avatar of Mike Blake"
+              role="img"
+            />
+          </div>
+        </div>
+
+        <div role="listitem" className="bg-white shadow rounded p-4 xl:p-8">
+          <BiSolidQuoteSingleLeft></BiSolidQuoteSingleLeft>
+          <div className="pl-4 pt-4 flex items-start justify-between">
+            <div className="mr-6">
+              <p className="xl:text-xl xl:leading-loose text-gray-600">
+                Proud to be a part of FoodUnitys mission for a greener, kinder
+                world.
+              </p>
+              <p className="mt-4 text-base font-semibold leading-none text-gray-800">
+                David{" "}
+              </p>
+            </div>
+            <img
+              src="https://i.ibb.co/JdVVPk9/avater4.png"
+              alt="Display avatar of David"
+              role="img"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Testimonial;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner";
 import Stat from "../../components/Stat";
+import Testimonial from "../../components/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Stat></Stat>
+      <Testimonial></Testimonial>
     </div>
   );
 };
