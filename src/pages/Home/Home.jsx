ecbe47dc065a56cc7330a230f84f5517c6d49ee1
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-05 18:15:35 +0600

    add statistics section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner";
+import Stat from "../../components/Stat";
 
 const Home = () => {
   return (
     <div>
-       <Banner></Banner>
+      <Banner></Banner>
+      <Stat></Stat>
     </div>
   );
 };
