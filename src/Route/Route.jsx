8318b290774aee57fff0968c9f0d8aac8b18fb3c
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-05 07:33:03 +0600

    setup react with tailwind css

---
+++ src/Route/Route.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+const myCreatedRoute = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+        ]
+        
+    }
+])
+
+export default myCreatedRoute;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      <p>Hello, I am Home</p>
+    </div>
+  );
+};
+export default Home;
