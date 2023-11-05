da47e89073c9ec5a7ff2e5f73984815848226003
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-05 10:31:38 +0600

    implement authentications with provider setup

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,16 +1,50 @@
 import PropTypes from 'prop-types';
-import { createContext} from 'react';
+import { createContext, useEffect, useState} from 'react';
+import auth from '../config/firebase.config';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({ children }) => {
-    // const [user,setUser] = useState(null)
-    // const [loading, setLoading] = useState(true);
+    const [user,setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+    // create new user 
+    const createUser = (email, password) => {
+        setLoading(true);
+       return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // login funtionality 
+    const signIn = (email, password) => {
+        setLoading(true);
+        return  signInWithEmailAndPassword(auth,email,password);
+    }
+
+    // logout funtionality
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // observer 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unSubscribe();
+        }
+    },[])
 
 
     const authInfo = {
-        // user,
-        // loading
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
     }
     return (
         <AuthContext.Provider value={authInfo}>
