f2a4f18cb073789e59b0f7ca404f136faee05408
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-05 16:46:40 +0600

    add footer section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,7 +13,8 @@ const Banner = () => {
     <div className="">
       <Swiper
         pagination={{
-          dynamicBullets: true,
+                  dynamicBullets: true,
+                  el: ".swiper-pagination",
         }}
         modules={[Pagination]}
       >
@@ -24,10 +25,10 @@ const Banner = () => {
             alt=""
           />
           <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h2 className="text-5xl text-center whitespace-nowrap font-semibold">
+            <h2 className="text-2xl md:text-4xl lg:text-5xl text-center whitespace-nowrap font-semibold">
               Save Food, Share Food
             </h2>
-            <p className="text-xl font-medium text-center">
+            <p className="text-base sm:text-xl font-medium text-center">
               Contribute to a sustainable future by sharing and rescuing excess
               food in your community.
             </p>
@@ -41,11 +42,12 @@ const Banner = () => {
             alt=""
           />
           <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h2 className="text-5xl text-center whitespace-nowrap font-semibold">
-            Make a Difference Today
+            <h2 className="text-2xl md:text-4xl lg:text-5xl text-center whitespace-nowrap font-semibold">
+              Make a Difference Today
             </h2>
-            <p className="text-xl font-medium text-center">
-            Help us divert perfectly good food from going to waste and provide it to those in need.
+            <p className="text-base sm:text-xl font-medium text-center">
+              Help us divert perfectly good food from going to waste and provide
+              it to those in need.
             </p>
             <button className="btn block mx-auto btn-neutral">Know More</button>
           </div>
@@ -55,13 +57,14 @@ const Banner = () => {
             className="block hero-overlay opacity-50 rounded-md mx-auto w-full h-[75vh]  object-cover"
             src="https://i.ibb.co/5vfsZP3/portuguese-gravity-qq3h-T-CYD1-I-unsplash.jpg"
             alt=""
-                  />
-                   <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h2 className="text-5xl text-center whitespace-nowrap font-semibold">
-               Deprived of Food
+          />
+          <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <h2 className="text-2xl md:text-4xl lg:text-5xl text-center whitespace-nowrap font-semibold">
+              Deprived of Food
             </h2>
-            <p className="text-xl font-medium text-center">
-            Connect with neighbors and local businesses to share surplus food and reduce waste.
+            <p className="text-base sm:text-xl font-medium text-center">
+              Connect with neighbors and local businesses to share surplus food
+              and reduce waste.
             </p>
             <button className="btn block mx-auto btn-neutral">Know More</button>
           </div>
@@ -71,19 +74,20 @@ const Banner = () => {
             className="block hero-overlay opacity-50 mx-auto rounded-md w-full h-[75vh]  object-cover"
             src="https://i.ibb.co/4J5Mck2/todd-quackenbush-x5-SRhk-Fajr-A-unsplash.jpg"
             alt=""
-                  />
-                   <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h2 className="text-5xl text-center whitespace-nowrap font-semibold">
+          />
+          <div className="absolute space-y-4 top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <h2 className="text-2xl md:text-4xl lg:text-5xl text-center whitespace-nowrap font-semibold">
               Ready to join hands ?
             </h2>
-            <p className="text-xl font-medium text-center">
-            Join the movement to reduce food waste and promote a more equitable, eco-friendly world.
+            <p className="text-base sm:text-xl font-medium text-center">
+              Join the movement to reduce food waste and promote a more
+              equitable, eco-friendly world.
             </p>
             <button className="btn block mx-auto btn-neutral">Know More</button>
           </div>
         </SwiperSlide>
           </Swiper>
-           
+          <div className="swiper-pagination absolute bottom-4 right-4 z-10" />
     </div>
   );
 };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
@@ -7,9 +8,10 @@ const MainLayout = () => {
             <div className="max-w-screen-xl mx-auto px-2 md:px-4 lg:px-8">
                 <Navbar></Navbar>
             </div>
-            <div className="max-w-screen-xl mx-auto px-2 md:px-4 lg:px-8">
+            <div className="min-h-screen max-w-screen-xl mx-auto px-2 md:px-4 lg:px-8">
             <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
