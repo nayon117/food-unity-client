d84fded44e19ea6d48f0b17c177b29aafedb1928
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-06 11:35:37 +0600

    implement search functionality

--- src/pages/AvailableFoods/AvailableFoods.jsx
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -3,22 +3,35 @@ import FeaturedCard from "../../components/FeaturedCard";
 
 const AvailableFoods = () => {
   const [availableFoods, setAvailableFoods] = useState([])
-  
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetch('http://localhost:5000/foods')
       .then(res => res.json())
        .then(data=>setAvailableFoods(data))
   }, [])
-  console.log(availableFoods);
+  
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Filter availableFoods based on the search query
+  const filteredFoods = availableFoods.filter((food) =>
+    food.foodName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+
   return (
     <div className="my-20">
       <input type="text" placeholder="search here ..." className="input input-bordered   block mx-auto
-      my-10 w-full max-w-xs" />
+      my-10 w-full max-w-xs"
+      onChange={handleSearchChange}  
+      value={searchQuery}
+      />
       <p className="text-4xl font-bold text-center">Available Foods</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {
-          availableFoods?.map(food => <FeaturedCard
+           filteredFoods?.map(food => <FeaturedCard
             key={food._id}
             food={food}
           ></FeaturedCard>)
