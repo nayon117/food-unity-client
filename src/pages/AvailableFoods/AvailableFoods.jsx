40d17915e677ec3da521bb630deb321a416b6f16
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-06 15:19:42 +0600

    implement sorting funcitonality

--- src/pages/AvailableFoods/AvailableFoods.jsx
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -2,17 +2,24 @@ import { useEffect, useState } from "react";
 import FeaturedCard from "../../components/FeaturedCard";
 
 const AvailableFoods = () => {
-  const [availableFoods, setAvailableFoods] = useState([])
+  const [availableFoods, setAvailableFoods] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
 
   useEffect(() => {
-    fetch('http://localhost:5000/foods')
-      .then(res => res.json())
-       .then(data=>setAvailableFoods(data))
-  }, [])
-  
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+    fetch(`http://localhost:5000/foods?sort=${sortOrder}`)
+      .then((res) => res.json())
+      .then((data) => setAvailableFoods(data));
+  }, [sortOrder]);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  //  handlesort functionality
+  const handleSort = () => {
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(newSortOrder);
   };
 
   // Filter availableFoods based on the search query
@@ -20,22 +27,26 @@ const AvailableFoods = () => {
     food.foodName.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-
   return (
     <div className="my-20">
-      <input type="text" placeholder="search here ..." className="input input-bordered   block mx-auto
-      my-10 w-full max-w-xs"
-      onChange={handleSearchChange}  
-      value={searchQuery}
-      />
+      <div className="flex items-center my-10">
+        <input
+          type="text"
+          placeholder="search here ..."
+          className="input input-bordered   block mx-auto
+        w-full max-w-xs"
+          onChange={handleSearchChange}
+          value={searchQuery}
+        />
+        <button className="btn  btn-neutral" onClick={handleSort}>
+          Sort by Expire Date
+        </button>
+      </div>
       <p className="text-4xl font-bold text-center">Available Foods</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {
-           filteredFoods?.map(food => <FeaturedCard
-            key={food._id}
-            food={food}
-          ></FeaturedCard>)
-        }
+        {filteredFoods?.map((food) => (
+          <FeaturedCard key={food._id} food={food}></FeaturedCard>
+        ))}
       </div>
     </div>
   );
