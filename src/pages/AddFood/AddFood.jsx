8d27d14e04d2ddf97c1c06b3017498d5875f4802
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-07 12:07:32 +0600

    create form in add food page

--- src/pages/AddFood/AddFood.jsx
+++ src/pages/AddFood/AddFood.jsx
@@ -1,7 +1,114 @@
+import useAuth from "../../hooks/useAuth";
+
 const AddFood = () => {
+
+  const { user } = useAuth()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const email = user.email;
+    const photo = user.photoURL;
+    const userName = user.displayName;
+    const foodName = form.foodName.value;
+    const foodImage = form.foodImage.value;
+    const foodQuantity = form.foodQuantity.value;
+    const pickupLocation = form.pickupLocation.value;
+    const expiredDateTime = form.expiredDateTime.value;
+    const foodStatus = form.foodStatus.value;
+    console.log(email, photo,userName, foodName, foodImage, foodQuantity, pickupLocation ,expiredDateTime, foodStatus);
+  }
+
+
+
+
+
+
   return (
-    <div>
-      <p>Hello, I am AddFood</p>
+    <div className="px-24 py-10 bg-[#F4F3F0]">
+      <h2 className="text-center font-bold text-2xl py-10">Add a Food</h2>
+      <form onSubmit={handleSubmit}>
+        {/* row */}
+        <div className="md:flex mb-6">
+          <div className="  md:w-1/2">
+            <label className="label" htmlFor="foodName">
+              <span className="label-text">Food Name</span>
+            </label>
+
+            <input
+              className="input w-full input-bordered"
+              name="foodName"
+              placeholder="Food Name"
+            />
+          </div>
+          <div className="md:w-1/2 md:ml-4">
+            <label className="label" htmlFor="foodImage">
+              <span className="label-text">Food Image</span>
+            </label>
+            <input
+              className="input w-full input-bordered"
+              name="foodImage"
+              placeholder="photo url"
+               
+            />
+          </div>
+        </div>
+        {/*  quantity pickup location */}
+        <div className="md:flex mb-6">
+          <div className="  md:w-1/2">
+            <label className="label" htmlFor="foodQuantity">
+              <span className="label-text">Quantity</span>
+            </label>
+
+            <input
+              className="input w-full input-bordered"
+              name="foodQuantity"
+              placeholder="quantity"
+            />
+          </div>
+          <div className=" md:w-1/2 md:ml-4">
+            <label className="label" htmlFor="pickupLocation">
+              <span className="label-text">Pickup Location</span>
+            </label>
+            <input
+              className="input w-full input-bordered  "
+              name="pickupLocation"
+              placeholder="Enter Pickup Location"
+            />
+          </div>
+        </div>
+        {/* expiredDateTime Additional Notes */}
+        <div className="md:flex mb-6">
+          <div className=" md:w-1/2">
+            <label className="label" htmlFor="expiredDateTime">
+              <span className="label-text">Expire Date</span>
+            </label>
+
+            <input
+              className="input w-full input-bordered"
+              type="date"
+              name="expiredDateTime"
+              placeholder=" expiredDateTime"
+            />
+          </div>
+          <div className=" md:w-1/2 md:ml-4">
+            <label className="label" htmlFor="foodStatus">
+              <span className="label-text">Food Status</span>
+            </label>
+            <input
+              className="input w-full input-bordered  "
+              name="foodStatus"
+             defaultValue='available'
+            />
+          </div>
+        </div>
+        
+        <input
+          className="btn btn-block my-3 btn-neutral"
+          type="submit"
+          value="Add Food"
+        />
+      </form>
     </div>
   );
 };
